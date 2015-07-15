a02ef95974453a115da8befb110dc7331bdf8cb7
empyrean987 <knoxm1@gmail.com>
2015-07-14 21:19:37 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,5 +21,4 @@ else
   rpm -ivh https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.6.2-1.el6.x86_64.rpm
 fi
 echo "Pulling down chef cookbooks for Graphite"
-git clone git://github.com/chef/chef-repo.git ~/chef_code
 chef-solo -c ~/chef_code/chef-solo/solo.rb
