e5866d0e8e922466388abfe419225076ff830398
empyrean987 <knoxm1@gmail.com>
2015-07-14 19:55:17 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 echo "YO YO"
+echo "Updating OS"
+sudo yum update -y
 echo "installing git"
 sudo yum install -y git
-echo "installing chef"
-rpm -ivh https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.6.2-1.el6.x86_64.rpm
+rpm -qa | grep chef > chef.txt
+if [ -s chef.txt ]
+then
+  echo " chef is installed"
+else
+  echo "installing chef"
+  rpm -ivh https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.6.2-1.el6.x86_64.rpm
+fi
 mkdir ~/chef_code/chef-repo
 echo "Pulling down chef cookbooks for Graphite"
+rm -rf ~/chef_code
 git clone git://github.com/chef/chef-repo.git ~/chef_code
 chef-solo -c ~/chef_code/chef-solo/solo.rb
