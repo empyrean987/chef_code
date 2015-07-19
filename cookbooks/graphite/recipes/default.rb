413ba72f3cedbcb141fe4278f47646ed5ca8a7e4
empyrean987 <knoxm1@gmail.com>
2015-07-18 22:34:19 -0500

    Update default.rb

--- cookbooks/graphite/recipes/default.rb
+++ cookbooks/graphite/recipes/default.rb
@@ -3,39 +3,89 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-package 'httpd'
-package 'mod_wsgi-python27.x86_64'
-package 'python27-pycairo.x86_64' do
+# Installing Apache 2.2
+package 'httpd-2.2.29-1.5.amzn1'
+
+#Install WSGI for Apapche, required to run Graphite WSGI scripts
+package 'mod_wsgi-python27-3.2-6.11.amzn1'
+
+#Installing Cairo from epel repository
+package 'python27-pycairo-1.8.6-2.1.11.amzn1' do
   options "--enablerepo=epel"
 end
-package 'python27-pycairo-devel.x86_64' do
+
+#Installing Cairo Development Packages from epel repository
+package 'python27-pycairo-devel-1.8.6-2.1.11.amzn1' do
   options "--enablerepo=epel"
 end
-package 'Django14.noarch' do
+
+#Installing Twisted from epel repository
+package 'python27-twisted-8.2.0-3.1.3.amzn1' do
   options "--enablerepo=epel"
 end
-package 'python-django-tagging.noarch' do
-  options "--enablerepo=epel"
+
+#GCC is required for Pytz, installing
+package 'gcc-4.8.2-3.19.amzn1'
+
+#Installing Pytz
+package 'python27-pytz-2010h-2.6.amzn1'
+
+#Decide to use pip for Django so we can only use python 2.7, using PIP
+#Installing Django
+execute 'django_install' do
+  command 'pip install django'
 end
-package 'python27-twisted.noarch' do
-  options "--enablerepo=epel"
+
+#Installing Django Tagging, same issue with Python 2.7, using PIP
+execute 'django-tagging_install' do
+  command 'pip install django-tagging'
 end
-package 'python-django15.noarch' do
-  options "--enablerepo=epel"
+
+#package 'pyparsing.noarch' do
+execute 'pyparsing_install' do
+  command 'pip install pyparsing'
 end
-package 'gcc'
-package 'python27-pytz.noarch'
-package 'pyparsing.noarch' do
-  options "--enablerepo=epel"
+
+#package 'python-whisper.noarch' do
+execute 'whisper_install' do
+  command 'pip install whisper'
 end
-package 'graphite-web.noarch' do
-  options "--enablerepo=epel"
+
+#Installing Graphite Web
+execute 'graphite-web_install' do
+  command 'pip install graphite-web'
 end
-package 'python-carbon.noarch' do
-  options "--enablerepo=epel"
+
+#Installing Carbon
+execute 'carbon_install' do
+  command 'pip install carbon'
 end
-package 'python-whisper.noarch' do
-  options "--enablerepo=epel"
+
+installed_file_path = "/root/chef_code/django_installed_correctly"
+uncompressed_file_dir = "/usr/local/lib/python2.7/site-packages/django/bin"
+execute "install database" do
+  command "/root/chef_code/django_install.sh"
+  cwd uncompressed_file_dir
+  not_if { File.exists?(installed_file_path) }
+end
+
+template '/etc/init.d/carbon-cache' do
+  source 'carbon-cache.erb'
+end
+
+service 'carbon-cache' do
+  supports :status => true
+  action [:enable]
+end
+
+template '/opt/graphite/conf/storage-aggregation.conf' do
+  source 'storage-aggregation.conf.erb'
+  notifies :restart, 'service[carbon-cache]'
+end
+
+service 'httpd' do
+  supports :status => true
+  action [:enable]
 end
 
 template '/etc/httpd/conf.d/graphite-web.conf' do
@@ -50,9 +100,7 @@ template '/etc/httpd/conf/httpd.conf' do
   source 'httpd.conf.erb'
 end
 
-template '/etc/init.d/carbon-cache' do
-  source 'carbon-cache.erb'
-end
+
 
 template '/var/www/error/error.html' do
   source 'error.html.erb'
@@ -73,23 +121,14 @@ template '/root/chef_code/django_install.sh' do
   mode '0755'
 end
 
-installed_file_path = "/root/chef_code/django_installed_correctly"
-uncompressed_file_dir = "/usr/lib/python2.6/site-packages/django/bin/"
-execute "install database" do
-  command "/root/chef_code/django_install.sh"
-  cwd uncompressed_file_dir
-  not_if { File.exists?(installed_file_path) }
-end
+
 
 file '/var/lib/graphite-web/graphite.db' do
   owner 'apache'
   group 'apache'
 end
 
-service 'httpd' do
-  supports :status => true
-  action [:enable, :restart]
-end
+
 
 service 'carbon-cache' do
   supports :status => true
